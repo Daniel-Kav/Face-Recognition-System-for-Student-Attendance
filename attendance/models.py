59485069c0193088340d41a07670b1026e7e4cdb
daniel-kav <dankavatha01@gmail.com>
2024-09-23 19:59:53 +0300

    Setting Up Models for Attendance and Users

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+# attendance/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20, unique=True)
+    profile_image = models.ImageField(upload_to='profile_images')
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student, related_name='classes')
+
+    def __str__(self):
+        return self.name
+
+class AttendanceRecord(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    present = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.date} - {'Present' if self.present else 'Absent'}"
