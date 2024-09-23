1d14bec6b646b458f9fcea587372465253c78790
daniel-kav <dankavatha01@gmail.com>
2024-09-23 21:46:27 +0300

    train model view

--- attendance/views.py
+++ attendance/views.py
@@ -3,8 +3,13 @@
 from django.shortcuts import render, redirect
 from .models import Student
 from .utils import capture_student_image
+from .utils import train_face_recognizer
 
 def capture_image(request, student_id):
     student = Student.objects.get(student_id=student_id)
     capture_student_image(student.student_id, student.full_name)
     return redirect('attendance_list')
+
+def train_model(request):
+    train_face_recognizer()
+    return render(request, 'attendance/train_success.html')
