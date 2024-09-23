07e32fe13774510c02e64aac34a26f3e3e5c23ac
daniel-kav <dankavatha01@gmail.com>
2024-09-23 21:45:24 +0300

    Django views to capture images

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Student
+from .utils import capture_student_image
+
+def capture_image(request, student_id):
+    student = Student.objects.get(student_id=student_id)
+    capture_student_image(student.student_id, student.full_name)
+    return redirect('attendance_list')
